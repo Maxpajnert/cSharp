0b9916653f11ffe8671294ca59a74765edd36702
Maxpajnert <max.pajnert@elev.ga.ntig.se>
2022-08-26 14:28:30 +0200

    a

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Once upon a time");
+            Console.WriteLine("I was falling i love");
+            Console.WriteLine("Now I'm only falling apart");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Once upon a time \r\nI was falling in love \r\nNow I’m only falling apart");
+
+            String text = "Live and sleep";
+
+            Console.WriteLine(text);
+
+            Console.ReadLine();
+
+            Console.WriteLine("Skriv något");
+
+            string något = Console.ReadLine();
+
+           Console.WriteLine(något + "\r\n" + något + "\r\n" + något);
+        }
+    }
+}
